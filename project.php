61666d36046d07e26832cd734e38b1f14f735380
Harsha7654 <161643598+Harsha7654@users.noreply.github.com>
2024-03-10 18:26:23 +0000

    Update project.php

--- project.php
+++ project.php
@@ -7,20 +7,50 @@ session_start();
 
 /*$results = getAllCheeses(); */
 
-if(!isset($_SESSION["previousSearch"]))
+if(!isset($_SESSION["basket"]))
 {
-    $_SESSION["previousSearch"] = [];
+    $_SESSION["basket"] = [];
 }
 
-if(!isset($_REQUEST["search"]))
+// if they typed something into the search by name
+// results = getAllCheesesByName(whatever they typed)
+if (isset($_REQUEST["searchn"]) && $_REQUEST["searchn"] != "")
 {
-    $results = getAllCheeses();
+    $results = getAllCheesesByName($_REQUEST["searchn"]);
+}
+else if (isset($_REQUEST["searchhs"]) && $_REQUEST["searchhs"] != "")
+{
+    $results = getAllCheesesByHardorSoft($_REQUEST["searchhs"]);
+}
+else if (isset($_REQUEST["searcho"]) && $_REQUEST["searcho"] != "")
+{
+    $results = getAllCheesesByOrigin($_REQUEST["searcho"]);
+}
+else if (isset($_REQUEST["searchs"]) && $_REQUEST["searchs"] != "")
+{
+    $results = getAllCheesesByStrength($_REQUEST["searchs"]);
+}
+else if (isset($_REQUEST["searchp"]) && $_REQUEST["searchp"] != "")
+{
+    $results = getAllCheesesByPrice($_REQUEST["searchp"]);
 }
 else
 {
-    $search = $_REQUEST["search"];
-    $_SESSION["previousSearch"][] = $search;
-    $results = getAllCheesesByName($search);
+    $results = getAllCheeses();
+}
+
+$basket = $_SESSION["basket"];
+
+if(isset($_REQUEST["addtobasket"]))
+{
+    //we've added the cheese
+    //add them to basket
+    $basketId = $_REQUEST["addtobasket"];
+    $basketobj = getAllCheesesByID($basketId);
+    $basket[] = $basketobj;
+    $_SESSION["basket"] = $basket;
+    
+    //$_SESSION["basket"][] = $_REQUEST["addtobasket"];
 }
 
 require_once "../view/project_view.php";
